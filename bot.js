56ddacf3528f9c3f198081a2cb7b085f83a0e195
Ahmad Hassan Khan <98497427+mrahmadhassankhan@users.noreply.github.com>
2022-02-20 23:15:36 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-/*I Write this simple script in Java Script this will work both for chegg and for bartleby for chegg just make few changes and this will work.
+/*I Write this simple script in Java Script this will work both for chegg and for bartleby for bartleby just make few changes and this will work.
 This code is only for educational purposes only it don't promote any illegal activity and all the credit goes to @ahmadhassankhan that's me so 
 don't copy paste on another website without persmission,Legal action will be taken if someone do so.*/
 
@@ -10,9 +10,9 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 
 const client = new Discord.Client();
 
-global.username = 'LOGIN GOES HERE'
-global.password = 'PASS GOES HERE'
-global.discordlogin = 'BOT TOKEN GOES HERE'
+global.username = 'CHEGG LOGIN GOES HERE'
+global.password = 'CHEGG PASS GOES HERE'
+global.discordlogin = 'KEY GOES HERE'
 
 var queue = false;
 
@@ -35,24 +35,24 @@ const download = (url, destination) => new Promise((resolve, reject) => {
 });
 
 client.on('ready', () => {
-    login_bartleby()
+    login_chegg()
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
 
-    if (message.content.startsWith(`-b`)) {
+    if (message.content.startsWith(`-chegg`)) {
 
-        var s = message.content.replace('-b', '')
+        var s = message.content.replace('-chegg', '')
 		
-        if (!s.includes('https://www.bartleby.com/questions-and-answers/')) {
+        if (!s.includes('https://www.chegg.com/homework-help')) {
             const errorEmbed = new Discord.MessageEmbed()
                 .setColor('#C91019')
                 .setTitle('Error')
-                .setDescription('No! Bartleby links only!')
+                .setDescription('No! Chegg links only!')
                 .setThumbnail('https://images-ext-1.discordapp.net/external/9yiAQ7ZAI3Rw8ai2p1uGMsaBIQ1roOA4K-ZrGbd0P_8/https/cdn1.iconfinder.com/data/icons/web-essentials-circle-style/48/delete-512.png?width=461&height=461')
                 .setTimestamp()
-                .setFooter('Powered by Ahmad Hassan Khan(Github ->@mrahmadhassankhan)');
+                .setFooter('Powered by Ahmad Hassan Khan (@ahmadhassankhan -> Github');
             message.channel.send(message.author.toString(), {
                 embed: errorEmbed
             });
@@ -66,7 +66,7 @@ client.on('message', message => {
                 .setDescription('Wait your turn!')
                 .setThumbnail('https://images-ext-1.discordapp.net/external/9yiAQ7ZAI3Rw8ai2p1uGMsaBIQ1roOA4K-ZrGbd0P_8/https/cdn1.iconfinder.com/data/icons/web-essentials-circle-style/48/delete-512.png?width=461&height=461')
                 .setTimestamp()
-                .setFooter('Powered by Ahmad Hassan Khan(Github ->@mrahmadhassankhan)');
+                .setFooter('Powered by Ahmad Hassan Khan (@ahmadhassankhan -> Github');
             message.channel.send(message.author.toString(), {
                 embed: errorEmbed
             });
@@ -82,28 +82,24 @@ client.on('message', message => {
                 .setTitle('Processing')
                 .setDescription('Your request is being processed!')
                 .setTimestamp()
-                .setFooter('Powered by Ahmad Hassan Khan(Github ->@mrahmadhassankhan)');
+                .setFooter('Powered by Ahmad Hassan Khan (@ahmadhassankhan -> Github');
             var msg = message.channel.send(message.author.toString(), {
                 embed: processEmbed
             });
             await page[0].goto(url, {
                 waitUntil: 'networkidle2'
-            });/*
-      
-            await page[0].setViewport({ width: 1024, height: 800 });
+            });
             global.screenshot = await page[0].screenshot({
-            path: 'test.png',
+                path: 'test.png',
                 fullPage: true
             });
-            message.author.send("Page ScreenShot:", {
-                files: ['./test.png']
-            });
-            
-           const images = await page[0].evaluate(() => Array.from(document.images, e => e.src));
+
+
+            const images = await page[0].evaluate(() => Array.from(document.images, e => e.src));
             let result;
             var question = false;
-           for (let i = 0; i < images.length; i++) {
-                if (images[i].includes('s3.amazonaws.com')) {
+           /*for (let i = 0; i < images.length; i++) {
+                if (images[i].includes('media.cheggcdn.com')) {
                     result = await download(images[i], `image-${i}.png`);
 
                     if (result === true) {
@@ -124,14 +120,12 @@ client.on('message', message => {
                     }
                 }
             }*/
-    
-            
 
-            const element = await page[0].$('#qna-answer-container-id > div.styles__MyQuestionContainer-sc-1xvt7re-1.ehiCjA');
+            const element = await page[0].$('.question-body-text');
             element_property = await element.getProperty('innerHTML');
             const inner_html = await element_property.jsonValue();
 
-            const element2 = await page[0].$('#qna-answer-container-id > div.styles__MyAnswerContainer-sc-1xvt7re-11.iYfoFb');
+            const element2 = await page[0].$('.answer-body');
             element_property2 = await element2.getProperty('innerHTML');
             const inner_html2 = await element_property2.jsonValue();
 
@@ -139,17 +133,20 @@ client.on('message', message => {
                 if (err) return console.log(err);
 
             });
+            message.author.send("PAGE SCREENSHOT:", {
+                files: ['./test.png']
+            })
             message.author.send("HTML ANSWER:", {
                 files: ['./answer.html']
             })
- 
+
             const successEmbed = new Discord.MessageEmbed()
                 .setColor('#00F800')
                 .setTitle('Success')
                 .setDescription('Your request has been processed, check your DMs!')
                 .setThumbnail('https://images-ext-2.discordapp.net/external/OVUlwF6n8j6wANCkwDzG_Rb2ivqCd9bRF10DC2Z8lS0/https/s5.gifyu.com/images/ezgif.com-optimized7ce94c5d4a783cb.gif')
                 .setTimestamp()
-                .setFooter('Powered by Ahmad Hassan Khan(Github ->@mrahmadhassankhan)');
+                .setFooter('Powered by Ahmad Hassan Khan (@ahmadhassankhan -> Github');
             message.channel.send(message.author.toString(), {
                 embed: successEmbed
             });
@@ -160,7 +157,7 @@ client.on('message', message => {
 
 })
 client.login(discordlogin);
-async function login_bartleby() {
+async function login_chegg() {
 
     global.browser = await puppeteer.launch({
         headless: false,
@@ -172,9 +169,19 @@ async function login_bartleby() {
     var userAgent = require('user-agents');
 
     await page[0].setUserAgent(userAgent.toString());
-    console.log("Going to Bartley");
-    await page[0].goto("https://www.bartleby.com/login&redirect=https%3A%2F%2Fwww.bartley.com%2F", {
+    console.log("Going to Chegg");
+    await page[0].goto("https://www.chegg.com/auth?action=login&redirect=https%3A%2F%2Fwww.chegg.com%2F", {
         waitUntil: 'networkidle2'
     });
 
+    console.log("Logging in");
+    await page[0].type('#emailForSignIn', username, {
+        delay: 100
+    });
+    await page[0].type('#passwordForSignIn', password, {
+        delay: 100
+    });
+    await page[0].click('#eggshell-10 > form > div > div > div > footer > button'); 
+    console.log("Ready!");
 }
+
